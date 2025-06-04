c31238294f435ed88a7460534fca7dcf4ddf63eb
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-06-04 17:18:26 +0530

    form logic
    working on the form logic.

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,35 +1,66 @@
+"use client";
 import React from "react";
-const ContactForm = () => {
+import { useState } from "react";
+export default function ContactForm() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log("Full name: ", fullname);
+    console.log("Email: ", email);
+    console.log("Message: ", message);
+  };
+
   return (
     <>
-      <form className="py-4 mt-4 border-t flex flex-col gap-5">
+      <form
+        onSubmit={handleSubmit}
+        className="py-4 mt-4 border-t flex flex-col gap-5"
+      >
         <div>
           <label htmlFor="fullname">Full Name</label>
-          <input type="text" id="fullname" placeholder="John Doe" />
+          <input
+            onChange={(e) => setFullname(e.target.value)}
+            value={fullname}
+            type="text"
+            id="fullname"
+            placeholder="John Doe"
+          />
         </div>
 
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder="john@gmail.com" />
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            id="email"
+            placeholder="john@gmail.com"
+          />
         </div>
 
         <div>
           <label htmlFor="message">Your Message</label>
           <textarea
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
             className="h-32"
             id="message"
             placeholder="Type your message here..."
           ></textarea>
         </div>
 
-         <button className="bg-green-700 p-3 text-white font-bold" type="submit">Send</button>
+        <button className="bg-green-700 p-3 text-white font-bold" type="submit">
+          Send
+        </button>
 
-          <div className="bg-slate-100 flex flex-col">
-            <div className="text-red-600 px-5 py-2"> error message</div>
-         </div>
-
+        <div className="bg-slate-100 flex flex-col">
+          <div className="text-red-600 px-5 py-2"> error message</div>
+        </div>
       </form>
     </>
   );
-};
-export default ContactForm;
+}
